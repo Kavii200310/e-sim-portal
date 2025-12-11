9e5538acb1ab071919b8656edf0ffe64d61b747e
buddhi vichakkshana <vichakkshanabuddhi@gmail.com>
2025-12-11 15:55:36 +0530

    Add payment feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+<<<<<<< Updated upstream
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+=======
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EligibilityPage from './features/eligibility/pages/EligibilityPage';
+import PaymentPage from './features/payment/pages/PaymentPage';
+import PaymentSuccessPage from './features/payment/pages/PaymentSuccessPage';
+import PaymentFailurePage from './features/payment/pages/PaymentFailurePage';
+
+import QrDeliveryPage from './features/wallet/pages/QrDeliveryPage';
+import ActivationPage from './features/wallet/pages/ActivationPage';
+import ActivationSuccessPage from './features/wallet/pages/ActivationSuccessPage';
+>>>>>>> Stashed changes
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+<<<<<<< Updated upstream
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -30,6 +43,20 @@ function App() {
       </p>
     </>
   )
+=======
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<EligibilityPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/payment/success" element={<PaymentSuccessPage />} />
+        <Route path="/payment/failure" element={<PaymentFailurePage />} />
+        <Route path="/wallet/qr-delivery" element={<QrDeliveryPage />} />
+        <Route path="/wallet/activation" element={<ActivationPage />} />
+        <Route path="/wallet/activation-success" element={<ActivationSuccessPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App
