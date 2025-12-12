b335b077e7cd841f53300df780f4fa7718febc3b
Kavii200310 <kaviiherath@gmail.com>
2025-12-12 13:22:20 +0530

    Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landingPage";
 import ChooseNumber from "./pages/ChooseNumber";
+import LoginPage from "./pages/LoginPage";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/choose-number" element={<ChooseNumber />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
   
   );
